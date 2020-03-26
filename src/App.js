bf5d5d826cace203aa0155861daf9900dfed4ead
Hakeem Elliot <hakeem.elliot@vaughn.edu>
2020-03-25 20:53:42 -0400

    created form backbone

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoList extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [],
+      currentToDo: "",
+    }
+  }
+  addItem = (event)=> {
+    event.preventDefualt();
+    console.log("this additme method executed");
+  };
+  handleChange = (event) =>{
+    console.log(event.target.value);
+  };
+  render() {
+    return(
+      <div className="container">
+      <form onsubmit={this.addItem}>
+      <label htmlFor="taskName">Task Name</label>
+      <input onchaange={this.handleChange} name="taskName" placeholder="Add to Do"/>
+      <button type="submit"> Add Task</button>
+      </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default ToDoList;
